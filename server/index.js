85677a177d124f6d3f731178705e71aa95223a68
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-06 22:20:51 +0530

    port fix

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 var server = http.createServer(app);
 var io = require("socket.io")(server);
 const Room = require("./models/room.js");
